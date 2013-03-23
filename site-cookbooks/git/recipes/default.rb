a6d5cd78cd07fa306853a5d2e574f036f057da12
fifnel <fifnel@gmail.com>
2013-03-23 10:51:31 +0900

    new file:   .vagrant 	modified:   nodes/vagrant.json 	new file:   site-cookbooks/git/CHANGELOG.md 	new file:   site-cookbooks/git/README.md 	new file:   site-cookbooks/git/metadata.rb 	new file:   site-cookbooks/git/recipes/default.rb 	new file:   site-cookbooks/ruby/CHANGELOG.md 	new file:   site-cookbooks/ruby/README.md 	new file:   site-cookbooks/ruby/metadata.rb 	new file:   site-cookbooks/ruby/recipes/default.rb
    gitとrubyの追加

---
+++ site-cookbooks/git/recipes/default.rb
@@ -0,0 +1,15 @@
+#
+# Cookbook Name:: git
+# Recipe:: default
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+package "git" do
+    action :install
+end
+package "tig" do
+    action :install
+end
+
